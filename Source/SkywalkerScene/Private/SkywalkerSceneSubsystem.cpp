a744af61533589b388454c7637ade7bae840a93d
Shyfan <410713847@qq.com>
2023-12-30 16:30:19 +0800

    增加流加载Level

--- Source/SkywalkerScene/Private/SkywalkerSceneSubsystem.cpp
+++ Source/SkywalkerScene/Private/SkywalkerSceneSubsystem.cpp
@@ -1,9 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "SkywalkerSceneSubsystem.h"
 
-bool USkywalkerSceneSubsystem::LevelTravel(const FString& InURL, bool bAbsolute, bool bShouldSkipGameNotify)
+#include "Kismet/GameplayStatics.h"
+
+bool USkywalkerSceneSubsystem::LoadLevel(const FName &InLevelName)
 {
-	return GetWorld()->ServerTravel(InURL, bAbsolute, bShouldSkipGameNotify);
+	UGameplayStatics::LoadStreamLevel(this, InLevelName, true, true, FLatentActionInfo());
+
+	return true;
+}
+
+bool USkywalkerSceneSubsystem::UnLoadLevel(const FName &InLevelName)
+{
+	UGameplayStatics::UnloadStreamLevel(this, InLevelName, FLatentActionInfo(), true);
+
+	return true;
 }
--- Source/SkywalkerScene/Public/SkywalkerSceneSubsystem.h
+++ Source/SkywalkerScene/Public/SkywalkerSceneSubsystem.h
@@ -7,15 +7,17 @@
 #include "SkywalkerSceneSubsystem.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class SKYWALKERSCENE_API USkywalkerSceneSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
-		
+
 public:
 	UFUNCTION(BlueprintCallable, Category = SkywalkerScene)
-		bool LevelTravel(const FString& InURL, bool bAbsolute = false, bool bShouldSkipGameNotify = false);
-	
+	bool LoadLevel(const FName &InLevelName);
+
+	UFUNCTION(BlueprintCallable, Category = SkywalkerScene)
+	bool UnLoadLevel(const FName &InLevelName);
 };
